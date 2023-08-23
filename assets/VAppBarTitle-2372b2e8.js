05f3561f90dee6ee2797e774e0f39fb3ecd707bb
flyangel22 <127477232+flyangel22@users.noreply.github.com>
2023-08-23 08:20:59 +0000

    Deploying to gh-pages from @ flyangel22/text-front@1cdde48efa920ae8eb5526ac0caaeb24a2db9afa 🚀

---
+++ assets/VAppBarTitle-2372b2e8.js
@@ -0,0 +1,1 @@
+import{b as o,c as s}from"./VMain-84e10bec.js";import{d as t,i as p,j as l,k as i}from"./index-b6681726.js";const T=t()({name:"VAppBarTitle",props:o(),setup(e,r){let{slots:a}=r;return p(()=>l(s,i(e,{class:"v-app-bar-title"}),a)),{}}});export{T as V};
